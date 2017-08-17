ee908fc56f4e47a2b03b0be2add6ccc7a831bdbe
JFPhilbin <jfphilbin@gmail.com>
2017-08-17 08:08:31 -0400

    fixed bug in isValidUid.

--- test/uid_test.dart
+++ test/uid_test.dart
@@ -26,7 +26,8 @@ List<String> goodUids = const <String>[
 List<String> badUids = const <String>[
   "3.2.840.10008.1.2.0", // "3.": not valid root
   "1.02.840.10008.1.2", // ".02": "0" can only be followed by "."
-  "1.2.840.10008.0.1.2." // ".2.": uid can't end with dot
+  "1.2.840.10008.0.1.2.", // ".2.": uid can't end with dot
+  "1.2.840.10a08.0.1.2"  // letters can't be used
 ];
 
 void uidTest() {
